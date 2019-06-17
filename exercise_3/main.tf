93c2caf659ba81bb9c739a921b5ad97b8323a10e
karl-evans <35782509+karl-evans@users.noreply.github.com>
2019-06-17 12:25:36 -0700

    Add files via upload

--- exercise_3/main.tf
+++ exercise_3/main.tf
@@ -1,10 +1,13 @@
 provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
   region = "${var.region}"
 }
 
 terraform {
    backend "s3" {
    bucket = "rc-tf-remote-state-bucket"
+   key = "terraform.tfstate"
    region = "us-east-1"
   }
 }
@@ -23,3 +26,4 @@ resource "aws_s3_bucket" "tf-root-module-bucket" {
     Environment = "${var.tag_env}"
   }
 }
+
